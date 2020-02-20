1328b07d758a798aeedabfdf1d7588d522a7fca9
iphyokafor <sandraifeoma22@gmail.com>
2020-02-20 01:38:53 +0100

    created a graphql server

---
+++ .history/server/server_20200220013138.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
