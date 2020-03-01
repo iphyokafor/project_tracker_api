46c7a14c625d465d0c644e66e5fbccb1729ca9c3
iphyokafor <sandraifeoma22@gmail.com>
2020-03-01 09:12:28 +0100

    created database connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema/schema';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+// Mongodb connection string
+mongoose.connect(
+    process.env.DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err) => {
+        if (err) throw err;
+        else console.log('connected to db!')
+    });
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
